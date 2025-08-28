c05e3eb1c71b7407eb5feaa3188a3c1736b6e107
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-28 19:46:51 +0600

    send this

---
+++ property.js
@@ -0,0 +1,9 @@
+const person={
+    name: 'Sohanur Rahman',
+    Age: 24,
+    Profession: 'Developer',
+    Salary: 25000,
+    Married: true
+}
+// console.log(person)
+console.log(person.Profession)  
