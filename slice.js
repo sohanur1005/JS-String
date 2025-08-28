02f988f854c9ced63309843a71ce38c04d6a1c03
sohanur24059 <sohanurrahman24059@gmail.com>
2025-08-28 13:54:52 +0600

    send this

---
+++ slice.js
@@ -0,0 +1,11 @@
+const address="Rajshahi"
+const part= address.slice(3,7)
+console.log(part)
+
+
+const sentence="I am a good working perosn";
+console.log(sentence.split())
+console.log(sentence.split(''))
+console.log(sentence.split(' '))
+
+
